e3bf9a7f895d2c74bf1e45d79f97c93c5128f058
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-06-04 10:08:53 +0430

    Add project files.

---
+++ Blazor_FileUpload/Services/FileUpload.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace Blazor_FileUpload.Services;
+
+public interface IFileUpload
+{
+    Task UploadFile(IBrowserFile file);
+}
+
+public class FileUpload : IFileUpload
+{
+    private IWebHostEnvironment _webHostEnvironment;
+    private readonly ILogger<FileUpload> _logger;
+
+    public FileUpload(IWebHostEnvironment webHostEnvironment, ILogger<FileUpload> logger)
+    {
+        _logger = logger;
+        _webHostEnvironment = webHostEnvironment;
+    }
+
+    public async Task UploadFile(IBrowserFile file)
+    {
+        if (file is not null)
+        {
+            try
+            {
+                var uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "uploads", file.Name);
+
+                using (var stream = file.OpenReadStream())
+                {
+                    var fileStream = File.Create(uploadPath);
+                    await stream.CopyToAsync(fileStream);
+                    fileStream.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+        }
+    }
+}

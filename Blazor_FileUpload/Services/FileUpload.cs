a6dfa6a80315eff94ec12e4ccb546364ae8c9a50
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-06-05 16:26:02 +0430

    imagePreview was added

--- Blazor_FileUpload/Services/FileUpload.cs
+++ Blazor_FileUpload/Services/FileUpload.cs
@@ -5,6 +5,7 @@ namespace Blazor_FileUpload.Services;
 public interface IFileUpload
 {
     Task UploadFile(IBrowserFile file);
+    Task<string> GeneratePreviewUrl(IBrowserFile file);
 }
 
 public class FileUpload : IFileUpload
@@ -38,5 +39,25 @@ public class FileUpload : IFileUpload
                 _logger.LogError(ex.Message);
             }
         }
+    }
+
+    public async Task<string> GeneratePreviewUrl(IBrowserFile file)
+    {
+        if (!file.ContentType.Contains("image"))
+        {
+            if (file.ContentType.Contains("pdf"))
+            {
+                return "images/pdf_logo.png";
+            }
+            else
+            {
+                return "images/txt_logo.png";
+            }
+        }
+
+        var resizedImage = await file.RequestImageFileAsync(file.ContentType, 100, 100);
+        var buffer = new byte[resizedImage.Size];
+        await resizedImage.OpenReadStream().ReadAsync(buffer);
+        return $"data:{file.ContentType};base64,{Convert.ToBase64String(buffer)}";
     }
 }
